204b104dcdf7939b7a271c7711030ebf18ecfefa
PSWindows <45151286+PSWindows@users.noreply.github.com>
2025-02-13 20:23:18 -0500

    Update WQLQuery.ps1

--- Tutorial03/WQLQuery.ps1
+++ Tutorial03/WQLQuery.ps1
@@ -9,3 +9,22 @@ $Results = Get-CimInstance -Namespace $SCCMNamespace -Query $Query
 
 $Results | Select-Object ResourceID, Name, Client, ResourceDomainORWorkgroup
 
+<#$Query = @"
+SELECT  
+    ResourceID,  
+    ResourceType,  
+    Name,  
+    SMSUniqueIdentifier,  
+    ResourceDomainORWorkgroup,  
+    Client  
+FROM  
+    SMS_R_System  
+WHERE  
+    ResourceID IN (  
+        SELECT ResourceID  
+        FROM SMS_G_System_OPERATING_SYSTEM  
+        WHERE Version LIKE '10.0%'  
+        AND BuildNumber < '22000'  
+        AND ProductType = '1'  
+    )
+"@#>
